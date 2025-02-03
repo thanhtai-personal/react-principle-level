bdcc21b71c7f8c0965232e1fbe9b18f16437e74c
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2025-02-03 21:14:28 +0700

    feat: config material-tailwind

---
+++ src/tailwind.config.js
@@ -0,0 +1,9 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+ 
+module.exports = withMT({
+  content: ["**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
